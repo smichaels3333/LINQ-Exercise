2c8ab6d1fb520d3933d7aa62381ee832c5ac2289
Stephen Michaels <stephen.michaels3333@gmail.com>
2023-03-03 11:19:17 -0500

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LINQ
-{
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{ 
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>() { "Halo Infinite", "Call of Duty", "Forza Horizon", "Hogwarts Legacy"};
+            var gameLength = videoGames.OrderBy(game=> game.Length);
+            Console.WriteLine(gameLength);
         }
     }
 }
